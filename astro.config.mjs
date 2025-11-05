edcebd8d2ebcd57ad1889dd423591ca65614954f
lucas.vasquez <lucasalfonso.vasquez@alumnos.ulagos.cl>
2025-11-04 21:28:53 -0300

    segundo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,12 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      tailwindcss({
-        config: {
-          content: [
-            './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-          ],
-          theme: {
-            extend: {
-              colors: {
-                'azul-oscuro': 'var(--color-azul-oscuro)',
-                'blanco': 'var(--color-blanco)',
-                'verde-neon': 'var(--color-verde-neon)',
-                'morado': 'var(--color-morado)',
-                'body': 'var(--color-body)',
-                'gris-claro': 'var(--color-gris-claro)',
-                'gris-oscuro': 'var(--color-gris-oscuro)',
-                'rojo': 'var(--color-rojo)',
-                'amarillo': 'var(--color-amarillo)',
-                'border-card': 'var(--color-border-card)',
-                'colorde-fondo': '#D2B48C',
-              },
-              fontFamily: {
-                'sans': ['var(--font-variable)', 'sans-serif'],
-                'roboto': ['var(--font-roboto)', 'sans-serif'],
-              }
-            },
-          },
-          plugins: [],
-        }
-      })
-    ]
+    plugins: [tailwindcss()]
   }
 });
 
